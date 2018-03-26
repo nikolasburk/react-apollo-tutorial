eacb5f47102184e5e313918e19328fc2f41728e9
Nikolas Burk <nikolas.burk@gmail.com>
2018-03-26 14:15:48 +0200

    update backend

--- server/src/index.js
+++ server/src/index.js
@@ -42,7 +42,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/public-warpcrow-597/my-react-basic/dev',
+      endpoint: '__PRISMA_ENDPOINT__',
       secret: 'mysecret123',
       debug: true,
     }),
